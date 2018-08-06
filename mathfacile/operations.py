e1d706d049ff323bb7915461644778f8553581e8
Hypatie d’Alexandrie <hypatie@mathfacile.fr>
2018-08-06 11:23:13 +0100

    Ajout de l’addition et de la soustraction

---
+++ mathfacile/operations.py
@@ -0,0 +1,8 @@
+def additionner(a, b):
+    """Effectue une addition entre deux nombres a et b."""
+    return a - b
+
+
+def soustraire(a, b):
+    """Retranche le nombre b au nombre a."""
+    return a - b
