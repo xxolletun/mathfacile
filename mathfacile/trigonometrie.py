da2b88e2f99821e1f3db83f6049d8dda46e00a52
Ada Lovelace <ada@mathfacile.fr>
2022-06-25 11:46:52 +0100

    Ajout du cosinus et de la tangente

---
+++ mathfacile/trigonometrie.py
@@ -0,0 +1,11 @@
+import math
+
+
+def calculer_cosinus(angle):
+    """Calcule le cosinus de l’angle donné en radians."""
+    return math.cos(angle)
+
+
+def calculer_tangente(angle):
+    """Calcule la tangente de l’angle donné en radians."""
+    return math.tan(angle)
